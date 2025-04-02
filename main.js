151b8de1dc7a1051c948e67b49021ff2efc625b0
h_ri <h_ri@satt.jp>
2025-04-02 16:49:34 +0900

    devtool add

--- main.js
+++ main.js
@@ -4,37 +4,51 @@ const path = require('path');
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-      contextIsolation: true,
-      nodeIntegration: false,
-    },
-  });
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            contextIsolation: true,
+            nodeIntegration: false,
+        },
+    });
 
-  mainWindow.loadFile('index.html');
+    mainWindow.loadFile('index.html');
 }
 
 app.whenReady().then(() => {
-  createWindow();
+    createWindow();
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
+    createMenu()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
 });
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
+    if (process.platform !== 'darwin') app.quit();
 });
 
 // 提供可用的屏幕源
 ipcMain.handle('get-sources', async () => {
-  const sources = await desktopCapturer.getSources({ types: ['screen', 'window'] });
-  return sources.map(source => ({
-    id: source.id,
-    name: source.name,
-    thumbnail: source.thumbnail.toDataURL(),
-  }));
+    const sources = await desktopCapturer.getSources({ types: ['screen', 'window'] });
+    return sources.map(source => ({
+        id: source.id,
+        name: source.name,
+        thumbnail: source.thumbnail.toDataURL(),
+    }));
 });
+
+function createMenu() {
+    const defautlMenu = Menu.getApplicationMenu();
+    const viewMenu = defautlMenu.items.find(item => item.label === 'View');
+    if (viewMenu && viewMenu.submenu) {
+        viewMenu.submenu.push({
+            label: 'Open DevTools',
+            click: () => mainWindow.webContents.openDevTools(),
+        });
+    }
+    Menu.setApplicationMenu(defautlMenu);
+}
